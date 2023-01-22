47405ced2b3f582808fe07adb6aa15c6aca3bd3c
khawajaazfar2000 <khawajaazfar2000@gmail.com>
2023-01-22 23:24:17 +0500

    Updates

---
+++ action_page.php
@@ -0,0 +1,28 @@
+<?php 
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$message = $_POST['message'];
+
+$conn - new mysqli('localhost', 'root', '', 'blog_website');
+if($conn->connect_error){
+    die('Connection Failed :' '.$conn->connect_error');
+}else{
+    $stmt = $conn->prepare("Insert init comments(firstname, lastname,email, gender, message) values(?,?,?,?,?,?)");
+    $stmt->bind_param("sssss",$firstname, $lastname, $email, $gender, $message);
+    $stmt->execute();
+    echo "Message Sent....";
+    $stmt->close();
+    $stmt->close();
+}
+
+
+
+
+
+
+
+
+
+?>
